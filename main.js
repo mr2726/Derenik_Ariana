7fa564c3bbbe311b3663213020fef6bdc33d3870
Aram <aru.aram99@gmail.com>
2025-06-11 11:33:18 -0700

    Connect TG Bot

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ passBtn.addEventListener('click', () => {
     wrapper.style.display = 'none';
     pass.style.display = 'block';
 });
+
+const sendBTN = document.getElementById('send');
+sendBTN.addEventListener('click', () => {
+    const token = '7572678936:AAFVDE7hZGMq3AI4LFV71gIfPYtAPzvWIu0';
+    const chatId = '5692567498';
+    const messageInput = document.getElementById('name');
+    const message = messageInput.value + " YES!";
+
+    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        chat_id: chatId,
+        text: message
+    })
+    })
+    .then(response => response.json())
+    .then(data => console.log('Message sent:', data))
+    .catch(error => console.error('Error:', error));
+});
